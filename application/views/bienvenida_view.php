dede8f2153673c755b2876d8cb4c6e2f8a5d07af
CrisRan <crisran2002@gmail.com>
2023-06-05 12:00:40 -0600

    clase 05-06-23

---
+++ application/views/bienvenida_view.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Bienvenido a Desarrollo web integral</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam quam tempora recusandae asperiores, saepe perspiciatis ea expedita exercitationem unde. Excepturi accusantium sunt ut beatae molestias, iure tenetur enim ad nobis.</p>
+</body>
+</html>
